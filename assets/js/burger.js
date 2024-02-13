b04836e3a9006c9677347b70d24b7967dffc5c74
yashindows <kiram.imav@gmal.com>
2024-02-13 22:58:23 +0900

    add adaptive layout main page tbe

---
+++ assets/js/burger.js
@@ -0,0 +1,8 @@
+const icon = document.querySelector(".menu-icon")
+const menu = document.querySelector(".menu__body")
+
+icon.addEventListener("click", () => {
+  menu.classList.toggle("active")
+  icon.classList.toggle("active")
+  document.body.classList.toggle("lock")
+})
