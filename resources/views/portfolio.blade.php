ed2a420ce04ada89ada530278d3825bf5d0ce060
yashindows <kiram.imav@gmal.com>
2024-03-28 23:01:20 +0900

    common

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -14,6 +14,24 @@
             <p><b>Продолжительность:</b> 35 мин.</p>
           </div>
         </div>
+        <div class="service-item">
+          <img src="{{asset('img/beautiful-young-woman-going-through-microblading-treatment 2.png')}}" alt="" class="item__img">
+          <div class="item__title section-title-text">название услуги</div>
+          <div class="item-info section-item-text">
+            <p><b>Описание:</b> Текст какой то, пара слов тут будет об услуге. Буквально 2-3 предложения: как делают, какие плюсы там и т. д.</p>
+            <p><b>Цена:</b> 1000₽</p>
+            <p><b>Продолжительность:</b> 35 мин.</p>
+          </div>
+        </div>
+        <div class="service-item">
+          <img src="{{asset('img/beautiful-young-woman-going-through-microblading-treatment 2.png')}}" alt="" class="item__img">
+          <div class="item__title section-title-text">название услуги</div>
+          <div class="item-info section-item-text">
+            <p><b>Описание:</b> Текст какой то, пара слов тут будет об услуге. Буквально 2-3 предложения: как делают, какие плюсы там и т. д.</p>
+            <p><b>Цена:</b> 1000₽</p>
+            <p><b>Продолжительность:</b> 35 мин.</p>
+          </div>
+        </div>
       </div>
       <button onclick="show()" class="btn service-btn section-title-text">
         Онлайн запись
